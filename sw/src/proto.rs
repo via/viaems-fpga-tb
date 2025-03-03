0315c9506b27ffdb789ef77270094805171141a8
Matthew Via <via@matthewvia.info>
2025-03-02 20:54:12 -0500

    simpler input format

--- sw/src/proto.rs
+++ sw/src/proto.rs
@@ -84,76 +84,25 @@ impl DeviceCommand {
 pub fn parse_scenario_inputs(file: File) -> Vec<DeviceCommand> {
     let mut line = String::new();
     let mut reader = io::BufReader::new(file);
-    let mut adc_values = [0 as f64; 16];
-    let mut accumulated_delay = 0;
     let mut commands : Vec<DeviceCommand> = vec![];
-
-    let mut current_time = 0;
-    let mut last_command_time = 0;
 
     while reader.read_line(&mut line).unwrap() != 0 {
         let tokens : Vec<&str> = line.split_whitespace().collect();
         match tokens[0] {
-            "a" => { 
-                let cmd_delay : usize = (tokens[1].parse::<f64>().unwrap() * (15.0 / 4.0)) as usize;
-                let cmd_adc : Vec<f64> = tokens[2..].iter().map(|t| t.parse().unwrap()).collect();
-
-                // Iterate through pairs of adc values
-                let mut this_commands = vec![];
-                for sel in 0..7 {
-                    if cmd_adc[sel*2..sel*2+2] != adc_values[sel*2..sel*2+2] {
-                        let adc1 : u16 = ((cmd_adc[sel * 2] / 5.0) * 4095.0) as u16;
-                        let adc2 : u16 = ((cmd_adc[sel * 2 + 1] / 5.0) * 4095.0) as u16;
-
-                        let sel = sel as u8;
-                        this_commands.push(DeviceCommand::Adc{sel, adc1, adc2});
-                    }
-                }
-
-                let delay_from_tb_cmds = this_commands.len();
-                // If delay is greater than can be stored in a single ADC,
-                // generate delay commands. If delay is less than the number of commands
-                // , we need to borrow from next command,
-                // and if that one is also zero then we cannot actually manifest it
-
-                if this_commands.len() > 0 {
-                    let mut remaining_delay = cmd_delay + accumulated_delay;
-                    accumulated_delay = 0;
-                    if cmd_delay < delay_from_tb_cmds {
-                        panic!("Unrealizable command, delay too low");
-                    }
-                    while remaining_delay > delay_from_tb_cmds {
-                        let this_delay = std::cmp::min(remaining_delay, DeviceCommand::MAX_DELAY);
-                        commands.push(DeviceCommand::Delay((this_delay - 1) as u32));
-                        remaining_delay -= this_delay;
-                    }
-                    commands.append(&mut this_commands);
-                    adc_values.copy_from_slice(cmd_adc.as_slice());
-                } else {
-                    accumulated_delay += cmd_delay;
-                }
+            "d" => {
+                let delay : u32 = tokens[1].parse().unwrap();
+                commands.push(DeviceCommand::Delay(delay));
             },
-            "t" => {
-                let cmd_delay : usize = (tokens[1].parse::<f64>().unwrap() * (15.0 / 4.0)) as usize;
-                let this_triggers : u8 = if tokens[2].parse::<u8>().unwrap() == 0 { 5 } else { 10 };
-
-                let mut remaining_delay = accumulated_delay + cmd_delay;
-
-                // We need a rising and falling edge, minimum two cycles
-                if remaining_delay < 2 {
-                    panic!("Bad Trigger delay!");
-                }
-                remaining_delay -= 2;
-
-                while remaining_delay > 0 {
-                    let this_delay = std::cmp::min(remaining_delay, DeviceCommand::MAX_DELAY);
-                    commands.push(DeviceCommand::Delay((this_delay - 1) as u32));
-                    remaining_delay -= this_delay;
-                }
-                // MSB is NRST, always keep it high
-                commands.push(DeviceCommand::Output{delay: 0, outputs: 0x80 | this_triggers});
-                commands.push(DeviceCommand::Output{delay: 0, outputs: 0x80});
-                accumulated_delay = 0;
+            "a" => { 
+                let sel : u8 = tokens[1].parse().unwrap();
+                let adc1 : u16 = tokens[2].parse().unwrap();
+                let adc2 : u16 = tokens[3].parse().unwrap();
+                commands.push(DeviceCommand::Adc{sel, adc1, adc2});
+            },
+            "o" => {
+                let delay : u32 = tokens[1].parse().unwrap();
+                let outputs : u8 = tokens[2].parse().unwrap();
+                commands.push(DeviceCommand::Output{delay, outputs});
             },
             _ => { },
         }
@@ -175,7 +124,7 @@ pub fn collapse_outputs(responses: Vec<DeviceResponse>) -> Vec<OutputChange> {
     let mut result = vec![];
     for resp in responses {
         if let DeviceResponse::OutputChanged { delay, outputs } = resp {
-            time += delay;
+            time += delay + 1;
             if outputs != values {
                 values = outputs;
                 result.push(OutputChange{time, outputs});
